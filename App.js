0a688faf2b99c67c1f60950549ce8653f8927be6
ilonatr <ilona.tran@metropolia.fi>
2020-08-31 02:38:42 +0300

    http-b task

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-/* eslint-disable max-len */
-import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
-// import {StyleSheet, Text, SafeAreaView, FlatList, TouchableOpacity, Image} from 'react-native';
+/* eslint-disable no-unused-vars */
 import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, SafeAreaView, View, Text, Image} from 'react-native';
 import List from './components/List';
 
 const App = () => {
@@ -18,99 +17,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20,
   },
+  list_items: {
+    backgroundColor: 'lightgrey',
+    padding: 15,
+    margin: 3,
+  },
+  items_sidetoside: {
+    flexDirection: 'row',
+  },
+
 });
 
 export default App;
-
-/* const mediaArray = [
-  {
-    'key': '0',
-    'title': 'Title 1',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/161',
-    },
-    'filename': 'http://placekitten.com/2048/1920',
-  },
-  {
-    'key': '1',
-    'title': 'Title 2',
-    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/164',
-    },
-    'filename': 'http://placekitten.com/2041/1922',
-  },
-  {
-    'key': '2',
-    'title': 'Title 3',
-    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/167',
-    },
-    'filename': 'http://placekitten.com/2039/1920',
-  },
-];
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={mediaArray}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity style={styles.row}>
-              <Image
-                style={styles.image}
-                source={{uri: item.thumbnails.w160}}
-              />
-              <SafeAreaView style={styles.text}>
-                <Text style={styles.title}>{item.title}</Text>
-                <Text>{item.description}</Text>
-              </SafeAreaView >
-            </TouchableOpacity>
-          );
-        }}
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-};
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 10,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  row: {
-    flex: 1,
-    maxWidth: '100%',
-    height: 300,
-    flexDirection: 'row',
-    backgroundColor: 'lightgrey',
-    marginBottom: 5,
-  },
-  image: {
-    width: '50%',
-    marginTop: 10,
-    marginBottom: 10,
-    marginRight: 5,
-  },
-  text: {
-    maxWidth: '50%',
-    marginRight: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: 'lightgrey',
-    paddingRight: 5,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-});
-export default App; */
