f53b6d772fe08064419c999477bd34017d99ed86
ilonatr <ilona.tran@metropolia.fi>
2020-08-31 02:27:22 +0300

    design edits

--- components/ListItem.js
+++ components/ListItem.js
@@ -8,12 +8,12 @@ const ListItem = (props) => {
     <TouchableOpacity
       style={styles.list_items}>
       <View style={styles.items_sidetoside}>
-        <Image style={{width: 150, height: 150, borderRadius: 100}}
+        <Image style={{width: 150, height: 150, borderRadius: 100, marginRight: 20}}
           source={{uri: props.singleMedia.thumbnails.w160}}
         />
         <View style={styles.text}>
           <Text
-            style={styles.header}>{props.singleMedia.title}</Text>
+            style={styles.title}>{props.singleMedia.title}</Text>
           <Text
             style={{textAlign: 'left', width: 180}}>{props.singleMedia.description}</Text>
         </View>
@@ -24,22 +24,22 @@ const ListItem = (props) => {
 
 const styles = StyleSheet.create({
   list_items: {
-    backgroundColor: '#D4D4D4',
-    padding: 15,
-    margin: 3,
+    backgroundColor: 'lightgrey',
+    padding: 20,
+    margin: 10,
     borderStyle: 'solid',
-    borderWidth: 2,
+    borderWidth: 1,
     borderColor: 'grey',
-    borderRadius: 10,
+    borderRadius: 20,
   },
   items_sidetoside: {
     flexDirection: 'row',
   },
-  header: {
+  title: {
     fontSize: 20,
     fontWeight: 'bold',
-    paddingBottom: 20,
-    color: '#E394CE',
+    paddingBottom: 10,
+    color: 'brown',
   },
 });
 
