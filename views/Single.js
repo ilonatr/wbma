b046fd8f8ec898af0172c99c27dea9ca90c97667
ilonatr <ilona.tran@metropolia.fi>
2020-09-02 13:18:37 +0300

    last single edits

--- views/Single.js
+++ views/Single.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text, Image} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Image, View} from 'react-native';
 import PropTypes from 'prop-types';
 
+const mediaUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
 
 const Single = ({route}) => {
   console.log('route', route);
   const {file} = route.params;
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={{uri: file.filename}} />
-      <Text>{file.title}</Text>
-      <Text>{file.description}</Text>
-
-    </SafeAreaView>
+      <View style={styles.imagebox}>
+        <Image style={styles.image} source={{uri: mediaUrl + file.filename}} />
+        <Text style={styles.title} >{file.title}</Text>
+        <Text>{file.description}</Text>
+      </View>
+    </SafeAreaView >
   );
 };
 
@@ -25,7 +27,21 @@ const styles = StyleSheet.create({
     paddingTop: 40,
   },
   description: {
-
+    padding: 10,
+    alignItems: 'center',
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    padding: 10,
   },
 });
 
